29129c83bcb9edbcbc140272fe28500900a950d5
mmkhl <pmisha1014@gmail.com>
2023-03-03 17:35:10 +0600

    create some architecture

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,48 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <div class="wrapper">
+      <div class="container">
+        <nav>
+          <router-link to="/">Weather</router-link> |
+          <router-link to="/about">Recomendation</router-link>
+        </nav>
+        <router-view />
+      </div>
+    </div>
   </div>
 </template>
 
 <style>
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #ffffff;
 }
 
+.wrapper {
+  width: 100%;
+  height: 100vh;
+  background: rgb(42, 42, 43);
+}
+
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+}
 nav {
+  text-align: end;
   padding: 30px;
 }
 
 nav a {
+  padding: 0 30px;
+
   font-weight: bold;
-  color: #2c3e50;
+  color: #ffffff;
 }
 
 nav a.router-link-exact-active {
